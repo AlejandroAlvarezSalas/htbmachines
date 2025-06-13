bbf168a3f5b0f538adbe1517aacacb590b398681
Alejandro Alvarez on kali <alvarezsalas.alejandro@gmail.com>
2025-06-12 22:57:30 -0400

    basic menu and update function with dependencies checker

--- htbmachines.sh
+++ htbmachines.sh
@@ -10,8 +10,13 @@ purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
 
+#Global variables
+main_url="https://htbmachines.github.io/bundle.js"
+parameter_counter=0
+missing_dependencies=()
+
 function ctrl_c(){
-	echo -e "\n${redcColour}[!]${endColour} Exitting..."
+	echo -e "\n${redColour}[!]${endColour} Exitting..."
 	exit 1
 }
 #Ctrl+C
@@ -19,28 +24,69 @@ trap ctrl_c INT
 
 
 function helpPanel(){
-echo -e "\n[+] Uso:" 
+	echo -e "\n${yellowColour}[+]${endColour}${greyColour}Uso:${endColour}"
+	echo -e "\n\t${purpleColour}m)${endColour}${greyColour} Busca por nombre de máquina${endColour}"
+	echo -e "\n\t${purpleColour}u)${endColour}${greyColour} Actualiza el listado${endColour}"
+	echo -e "\n\t${purpleColour}h)${endColour}${greyColour} Muestra el panel de ayuda${endColour}"
 }
 
 function searchMachine(){
-   machineName="$1"
-   echo "hi $machineName"
+	machineName="$1"
+	if [ ! -f machine_list.txt ]
+	then
+		echo -e "\n${yellowColour}[+]${endColour}${greyColour}Lista no encontrada${endColour}"
+		updateList
+	fi
+	cat machine_list.txt | grep $machineName | head -n 3
 }
 
-#Indicadores
-declare -i parameter_counter=0
+function dependenciesChecker(){
+        if ! command -v sponge 1>/dev/null
+        then 
+                missing_dependencies[0]='sponge'
+        fi
+        if ! command -v js-beautify 1>/dev/null
+        then
+		missing_dependencies+=('beautify')
+        fi
 
-while getopts "m:h" arg; do
+}
+
+function updateList(){
+
+	dependenciesChecker
+
+	if (( ! ${#missing_dependencies[@]} ))
+	then
+		curl -s -X GET $main_url > machine_list.txt
+		js-beautify ./machine_list.txt | sponge machine_list.txt
+		echo -e "${yellowColour}[+]${endColour}${greyColour}Lista actualizada${endColour}"
+	else
+		echo -e "${yellowColour}[+]${endColour}${greyColour}Por favor instale las dependencias no encontradas (${#missing_dependencies[*]}) :${endColour} ${redColour} ${missing_dependencies[@]}${endColour}"
+	fi
+		
+	
+}
+
+while getopts "m:hu" arg; do
 	case $arg in
-	   m) machineName=$OPTARG; let parameter_counter+=1;;
-	   h) helpPanel;;
+	   m)
+		machineName=$OPTARG; let parameter_counter+=1
+		;;
+	   u)
+		let parameter_counter+=2
+		;;
+	   h)
+		helpPanel
+		;;
 
 	esac
 done
 
 if [ $parameter_counter -eq 1 ]; then
    searchMachine $machineName
+elif [ $parameter_counter -eq 2 ]; then
+   updateList
 else
    helpPanel
 fi
-
