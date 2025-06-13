f7e9b030b2b2f291935b551ad1ebd1cd4dd8f64f
Alejandro Alvarez on kali <alvarezsalas.alejandro@gmail.com>
2025-06-13 07:05:05 -0400

    added a recover from backup feature and made a version checking to compare local and remote version of machine_list

--- htbmachines.sh
+++ htbmachines.sh
@@ -52,29 +52,65 @@ function dependenciesChecker(){
 
 }
 
+function versionChecker(){
+
+	if [ -f machine_list.tmp ]
+	then
+		
+		currentMd5=$(md5sum machine_list.txt | awk '{print $1}')
+		newMd5=$(md5sum machine_list.tmp | awk '{print $1}')
+
+		if [ "$currentMd5" == "$newMd5" ]
+		then
+			echo -e "No hay actualizaciones pendientes"
+		else
+			cp machine_list.txt machine_list.backup
+			cp machine_list.tmp machine_list.txt
+		fi
+
+	fi
+
+	rm machine_list.tmp
+
+}
+	
+function recoverBackup(){
+	if [ -f machine_list.backup ]
+	then
+		cp machine_list.backup machine_list.txt
+	else
+		echo -e "No se han encontrado backups accesibles"
+
+	fi
+}
+
 function updateList(){
 
 	dependenciesChecker
 
 	if (( ! ${#missing_dependencies[@]} ))
 	then
-		curl -s -X GET $main_url > machine_list.txt
-		js-beautify ./machine_list.txt | sponge machine_list.txt
+		curl -s -X GET $main_url > machine_list.tmp
+		js-beautify ./machine_list.tmp | sponge machine_list.tmp
 		echo -e "${yellowColour}[+]${endColour}${greyColour}Lista actualizada${endColour}"
 	else
 		echo -e "${yellowColour}[+]${endColour}${greyColour}Por favor instale las dependencias no encontradas (${#missing_dependencies[*]}) :${endColour} ${redColour} ${missing_dependencies[@]}${endColour}"
 	fi
-		
+
+	versionChecker
 	
 }
 
-while getopts "m:hu" arg; do
+while getopts "m:hur" arg; do
 	case $arg in
 	   m)
 		machineName=$OPTARG; let parameter_counter+=1
 		;;
 	   u)
 		let parameter_counter+=2
+		;;
+	   r)
+		let parameter_counter+=3
 		;;
 	   h)
 		helpPanel
@@ -83,10 +119,15 @@ while getopts "m:hu" arg; do
 	esac
 done
 
-if [ $parameter_counter -eq 1 ]; then
+if [ $parameter_counter -eq 1 ]
+then
    searchMachine $machineName
-elif [ $parameter_counter -eq 2 ]; then
-   updateList
+elif [ $parameter_counter -eq 2 ]
+then
+	updateList
+elif [ $parameter_counter -eq 3 ]
+then
+	recoverBackup
 else
-   helpPanel
+	helpPanel
 fi
